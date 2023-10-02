7e22ed55906cc56419bb659bedac4d2333d06ad7
ashish-pro <ashishbit017@outlook.com>
2023-10-02 19:55:45 +0530

    updates

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
-let clock = setInterval(clocktiming, 1000)
+
+const display = document.getElementById("showClock");
+
 function clocktiming(){
     let d = new Date();
     let x = d.toLocaleTimeString();
-    document.getElementById("showClock").innerHTML= x
+    display.innerHTML= x
 }
 
-let flag = 0
+let clock = setInterval(clocktiming, 1000)
 
-function play(){
-    if(flag == 0){
-        
-        document.getElementById("btnt").innerHTML = "Start the Watch"
-        clearInterval(clock);
-        console.log("if statement")
-        flag = 1
-    }
-    else{
-        setInterval(clocktiming, 1000)
-        document.getElementById("btnt").innerHTML = "Stop the Watch"
-        console.log("else statement")
-        flag = 0
-    }
-}
-
+// On Loading
+clocktiming();
 
